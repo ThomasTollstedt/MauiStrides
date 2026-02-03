cf9aefb5171aa34fb510cffb1d440edd65d8ec6d
Thomas Tollstedt <thomastollstedt@gmail.com>
2026-02-03 09:31:45 +0100

    Fix when token expired, app code behind to check Auth status.

--- MauiStrides/App.xaml.cs
+++ MauiStrides/App.xaml.cs
@@ -6,14 +6,34 @@ namespace MauiStrides
 {
     public partial class App : Application
     {
-        public App()
+        private readonly ITokenService _tokenService;
+
+        public App(ITokenService tokenService)
         {
             InitializeComponent();
+            _tokenService = tokenService;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            var mainPage = CheckAuthenticationStatus();
+            return new Window(mainPage);
+        }
+
+        private Page CheckAuthenticationStatus()
+        {
+            // Check if user has stored tokens
+            var hasTokens = Task.Run(async () => await _tokenService.HasStoredTokensAsync()).Result;
+
+            if (hasTokens)
+            {
+                return new AppShell();
+            }
+            else
+            {
+                var loginPage = IPlatformApplication.Current.Services.GetService<LoginPage>();
+                return new NavigationPage(loginPage);
+            }
         }
     }
 }
